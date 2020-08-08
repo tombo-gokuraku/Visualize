7e95b565a4135b0d5fa61aa6a2be194cbbd6bc48
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-08 19:08:42 +0900

    feat: Edit Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import tw, { styled } from "twin.macro"
 
 const Header = styled.header(() => [
-  tw`flex justify-between px-6 py-2 mb-6 bg-primary`,
+  tw`flex flex-col justify-between px-6 py-2 mb-6 text-center bg-transparent`,
 ])
 
 export default Header
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Header from "./Header"
 import Footer from "./Footer"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Header />
       <main>{children}</main>
       <Footer />
     </>
