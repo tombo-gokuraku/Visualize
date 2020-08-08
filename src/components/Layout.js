519be0d46082acd1208b576f45c026ab7720acf4
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-08 18:22:06 +0900

    chore: Remove unused components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,31 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import "twin.macro"
 
 import Header from "./Header"
 import Footer from "./Footer"
-import Title from "./Title"
-import Navigation from "./Navigation"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header>
-        <Title siteTitle={data.site.siteMetadata.title} />
-        <Navigation />
-      </Header>
+      <Header />
       <main>{children}</main>
       <Footer />
     </>
