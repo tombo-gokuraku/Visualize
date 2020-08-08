b121f2ee31efd64321daa45fc5c10f9f38514a83
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-08 19:34:33 +0900

    chore: Add FontAwesome Credit to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,20 @@ function Footer() {
     <footer tw="flex flex-col items-center justify-center px-4 py-12 md:(px-6 py-16) xl:(px-8 py-20) text-gray-700">
       <div tw="text-xs text-center">
         © {new Date().getFullYear()}, Built with{" "}
-        <a href="https://github.com/tombo-gokuraku/gatsby-starter-tailwind-lp">
+        <a
+          href="https://github.com/tombo-gokuraku/gatsby-starter-tailwind-lp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           gatsby-starter-tailwind-lp
+        </a>{" "}
+        500px icon:{" "}
+        <a
+          href="https://fontawesome.com/icons/500px"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fontawesome
         </a>
       </div>
     </footer>
