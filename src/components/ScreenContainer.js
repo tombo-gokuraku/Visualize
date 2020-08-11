26e16ae41c7d502bbb9511e2416102443a35eae6
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-11 15:13:47 +0900

    fix: Fix padding

--- src/components/ScreenContainer.js
+++ src/components/ScreenContainer.js
@@ -1,5 +1,5 @@
 import tw from "twin.macro"
 
-const ScreenContainer = tw.div`px-8 my-0 mx-auto lg:(px-0 max-w-screen-lg) xl:(max-w-screen-xl)`
+const ScreenContainer = tw.div`px-8 my-0 mx-auto lg:(max-w-screen-lg) xl:(max-w-screen-xl)`
 
 export default ScreenContainer
