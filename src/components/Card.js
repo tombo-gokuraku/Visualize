f30ab74a84b662ed7bcaa155d6f99aef90557459
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-17 16:00:28 +0900

    feat: Set react-modal to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,56 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core"
-// import React, { useState } from "react"
+import { useState } from "react"
 import PropTypes from "prop-types"
 import tw, { css } from "twin.macro"
+import Modal from "react-modal"
 
+import { MdClose } from "react-icons/md"
 import Image from "gatsby-image"
 import H2 from "../components/H2"
 
+Modal.setAppElement("#___gatsby")
+
+const modalStyles = {
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+  },
+  content: {
+    position: "relative",
+    top: "auto",
+    left: "auto",
+    right: "auto",
+    bottom: "auto",
+    maxWidth: "100%",
+    maxHeight: "100vh",
+    height: "100vh",
+    margin: "0 auto",
+    padding: "1rem",
+    border: 0,
+    backgroundColor: "transparent",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}
+
 function Card({ children, image, title }) {
+  const [showModal, setShowModal] = useState(false)
   return (
     <div
       css={[
-        tw`flex flex-col items-center justify-center w-full mb-10 rounded-lg`,
+        tw`flex flex-col items-center justify-center w-full mb-10 rounded-lg focus:outline-none`,
         css`
           box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.25);
           background-color: rgba(255, 255, 255, 0.075);
         `,
       ]}
+      onClick={() => setShowModal(true)}
+      onKeyDown={event => {
+        if (event.key === "Enter") setShowModal(true)
+      }}
+      role="button"
+      tabIndex={0}
     >
       <div tw="w-full rounded-t-lg">
         <Image
@@ -28,6 +62,29 @@ function Card({ children, image, title }) {
         />
       </div>
       <H2>{title}</H2>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={() => setShowModal(false)}
+        style={modalStyles}
+      >
+        <button
+          css={[
+            tw`absolute z-10 text-4xl text-white border-2 border-white rounded-full text-opacity-50 border-opacity-50`,
+            css`
+              top: 1rem;
+              right: 1rem;
+            `,
+          ]}
+          onClick={() => setShowModal(false)}
+        >
+          <MdClose />
+        </button>
+        <Image
+          fluid={image}
+          style={{ display: "inline-block", width: "100%", height: "100%" }}
+          imgStyle={{ objectFit: "contain", width: "100%", height: "100%" }}
+        />
+      </Modal>
     </div>
   )
 }
