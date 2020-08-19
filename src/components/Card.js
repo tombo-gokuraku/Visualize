37ad35a9c272ddee1da41066fe3aada42dbaa1ad
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-19 09:47:06 +0900

    fix: Fix animation css style. But background is not transparent.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core"
+import { jsx, Global } from "@emotion/core"
 // import { ClassNames } from "@emotion/core"
 import { useState } from "react"
 import PropTypes from "prop-types"
@@ -14,7 +14,8 @@ Modal.setAppElement("#___gatsby")
 
 const modalStyles = {
   overlay: {
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
+    // backgroundColor: "rgba(0, 0, 0, 0.0)",
+    // backgroundColor: "transparent",
   },
   content: {
     position: "relative",
@@ -35,18 +36,18 @@ const modalStyles = {
   },
 }
 
-const overlayAnimationStyles = {
-  base: {
-    backgroundColor: "rgba(0, 0, 0, 0)",
-    transition: "background 500ms ease-in-out",
-  },
-  afterOpen: {
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
-  },
-  beforeClose: {
-    backgroundColor: "rgba(0, 0, 0, 0)",
-  },
-}
+// const overlayAnimationStyles = {
+//   base: {
+//     backgroundColor: "rgba(0, 0, 0, 0)",
+//     transition: "background 500ms ease-in-out",
+//   },
+//   afterOpen: {
+//     backgroundColor: "rgba(0, 0, 0, 0.8)",
+//   },
+//   beforeClose: {
+//     backgroundColor: "rgba(0, 0, 0, 0)",
+//   },
+// }
 function Card({ children, image, title }) {
   const [showModal, setShowModal] = useState(false)
   return (
@@ -76,28 +77,50 @@ function Card({ children, image, title }) {
       </div>
       <H2>{title}</H2>
       {/* <ClassNames> */}
+      <Global
+        styles={css`
+          .overlay-base {
+            background-color: rgba(0, 0, 0, 0);
+            /* opacity: 0; */
+            transition: background 500ms ease-in-out;
+            /* transition: opacity 500ms ease-in-out; */
+          }
+          .overlay-after {
+            background-color: rgba(0, 0, 0, 0.8);
+            /* opacity: 1; */
+          }
+          .overlay-before {
+            background-color: rgba(0, 0, 0, 0);
+            /* opacity: 0; */
+          }
+        `}
+      />
       <Modal
         isOpen={showModal}
         onRequestClose={() => setShowModal(false)}
         style={modalStyles}
         closeTimeoutMS={500}
-        overlayClassName={overlayAnimationStyles}
-        css={[
-          css`
-            .ReactModal__Overlay {
-              background-color: rgba(0, 0, 0, 0);
-              transition: background 500ms ease-in-out;
-            }
+        overlayClassName={{
+          base: "overlay-base",
+          afterOpen: "overlay-after",
+          beforeClose: "overlay-before",
+        }}
+        //css={[
+        //  css`
+        //    .ReactModal__Overlay {
+        //      background-color: rgba(0, 0, 0, 0);
+        //      transition: background 500ms ease-in-out;
+        //    }
 
-            .ReactModal__Overlay--after-open {
-              background-color: rgba(0, 0, 0, 0.8);
-            }
+        //    .ReactModal__Overlay--after-open {
+        //      background-color: rgba(0, 0, 0, 0.8);
+        //    }
 
-            .ReactModal__Overlay--before-close {
-              background-color: rgba(0, 0, 0, 0);
-            }
-          `,
-        ]}
+        //    .ReactModal__Overlay--before-close {
+        //      background-color: rgba(0, 0, 0, 0);
+        //    }
+        //  `,
+        //]}
       >
         <button
           css={[
