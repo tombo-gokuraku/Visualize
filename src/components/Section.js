afee6799bc727a783c4a1f3e2f8cdaecceab7dab
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-19 16:08:30 +0900

    remove: Remove unused assets

--- src/components/Section.js
+++
@@ -1,8 +0,0 @@
-import tw, { styled } from "twin.macro"
-
-const Section = styled.section(({ reverse }) => [
-  tw`px-4 py-12 md:(py-16) xl:(py-20) bg-gray-100 text-base text-gray-900`,
-  reverse && tw`text-gray-900 bg-primary`,
-])
-
-export default Section
