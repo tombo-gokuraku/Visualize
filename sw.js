5950fbafafb63d6455f96be16eea976833fef315
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-23 12:02:55 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f76dbe9c0bc74742687b.js"
+    "url": "webpack-runtime-143a78867c7410936e3c.js"
   },
   {
     "url": "framework-c2d1b729e584de1ab8dd.js"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "styles-24c541b6ac347bae38f1.js"
   },
   {
-    "url": "app-f2f7e53ab34049f1a816.js"
+    "url": "app-7d3f961091e3456bbf85.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "876fca9b94eafdd81c2c6176e5783c70"
+    "revision": "9e45ea6aecb78b460cb17ab641c6425c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-61f38fd4f8f16e2fdffb.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "2c80c44f6e8c4a46253d0af0e888f075"
+    "revision": "57148e49ae5f5fad79c16ca03c5419da"
   },
   {
     "url": "polyfill-dee37dcd29fe05bc0cef.js"
@@ -153,7 +153,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/Visualize/app-f2f7e53ab34049f1a816.js`))) {
+  if (!resources || !(await caches.match(`/Visualize/app-7d3f961091e3456bbf85.js`))) {
     return await fetch(event.request)
   }
 
